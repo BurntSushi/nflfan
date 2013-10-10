a5bdec69c8ce6a7f85fc80accb6991acd5ffa97f
Andrew Gallant <jamslam@gmail.com>
2013-10-10 15:49:55 -0400

    adapt video player size

--- nflfan/web/js/video.js
+++ nflfan/web/js/video.js
@@ -1,5 +1,7 @@
 function hide_player() {
-    $('#vid video').get(0).pause();
+    var $vid = $('#vid video');
+    $vid.get(0).pause();
+    $vid.removeAttr('src');
     $('#vid').slideUp();
 }
 
@@ -7,6 +9,9 @@ function watch_play(gsis_id, play_id) {
     var url = make_vid_url(gsis_id, play_id);
     var $vid = $('#vid video');
     var vid = $vid.get(0);
+    
+    $vid.css({'width': 'auto', 'height': 'auto'});
+    $('#vid').css({'width': 'auto', 'height': 'auto'});
 
     $vid.attr('src', url);
     vid.load();
@@ -16,5 +21,20 @@ function watch_play(gsis_id, play_id) {
 }
 
 $(document).ready(function() {
+    var $div = $('#vid');
+    var $vid = $('#vid video');
+    var spacing = 100;
+    var $win = $(window);
     $('#vid button').click(hide_player);
+
+    window.setInterval(function() {
+        if ($div.width() >= ($win.innerWidth() - spacing)) {
+            $div.width($win.innerWidth() - spacing);
+            $vid.width($div.width());
+        }
+        if ($div.height() >= ($win.innerHeight() - spacing)) {
+            $div.height($win.innerHeight() - spacing);
+            $vid.height($div.height() - 50);
+        }
+    }, 500);
 });
